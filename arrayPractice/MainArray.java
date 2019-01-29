982e7fe6c632efb4ca0b1cf506d8c89847d6487f
domHenin <domhenin.517@gmail.com>
2019-01-28 23:53:44 -0500

    added array source file

---
+++ arrayPractice/MainArray.java
@@ -0,0 +1,13 @@
+public class MainArray {
+    public static void main(String[] args) {
+        //Interger Array
+        int values[] = {2, 4, 6, 8};
+        for(int value : values){
+            System.out.println(value);
+        }
+
+        //String Array
+        String words[] = {"Hello", "World"};
+        for (String word : words);
+    }
+}
