ad18e36d31b2bf78c73fd20ea57238dddca9465e
domHenin <domhenin.517@gmail.com>
2019-02-06 15:46:02 -0500

    played around with constructors without watching the tutorial

---
+++ SettersAndThisPractice/SettersThisMain.java
@@ -0,0 +1,42 @@
+class Frog {
+    String name;
+    int age;
+
+    //  created a String setter
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    //  created an int setter
+    public void setAge(int newAge){
+        age = newAge;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
+public class SettersThisMain {
+    public static void main(String[] args) {
+        Frog frogOne = new Frog();
+
+        frogOne.name = "Bertie"; /*setting an instance variable with an '=' sign*/
+        frogOne.age = 3;
+
+//      calling the setter
+        frogOne.setName("Bertie"); /*this is different from line24 because im calling an actual method*/
+    }
+}
+
+
+
+//TODO:
+//> use the 'encapsulation' method in this code
+//> set the String to private upon first call
+//> set the int to private upon first call
--- constructorPractice/MainConstructor.java
+++ constructorPractice/MainConstructor.java
@@ -1,12 +1,63 @@
-//class Vehicle {
-//    public void Vehicle() {
-//        System.out.println("Inside constructor");
-//    }
-//}
+class Vehicle {
+    private String carModel;
+    private String carMake;
+    private String carColor;
+
+    public Vehicle() {
+        carModel = "Acura";
+        carMake = "RSX";
+        carColor = "White";
+    }
+
+    public Vehicle(String carModel, String carMake, String carColor){
+        this.carModel = carModel;
+        this.carMake = carMake;
+        this.carColor = carColor;
+    }
+
+    public String getCarModel(){
+        return carModel;
+    }
+
+    public String getCarMake(){
+        return carMake;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void changeCarModel(String newCarModel){
+        carModel = newCarModel;
+    }
+
+    public void changeCarColor(String newCarColor) {
+        carColor = newCarColor;
+    }
+}
 
 
-//public class MainConstructor {
-//    public static void main(String[] args) {
-//        Vehicle vehicle = new Vehicle();
-//    }
-//}
+public class MainConstructor {
+    public static void main(String[] args) {
+//      System.out.println("hello world!");
+
+        Vehicle vehicle = new Vehicle();
+
+      System.out.println("Car Model: "+vehicle.getCarModel());
+      System.out.println("Car Make: "+vehicle.getCarColor());
+      System.out.println("Car Color: "+vehicle.getCarColor());
+
+        System.out.println("I want to change my cars color.....");
+        System.out.println("... Processing ...");
+
+        vehicle.changeCarColor("Black");
+        System.out.println("New Car Color: "+vehicle.getCarColor());
+
+    }
+}
+
+//TODO:
+//> create a 'car' class
+//> set in 'car' class a car name, car make, and car color
+//> call from main
+//> change the color of car
