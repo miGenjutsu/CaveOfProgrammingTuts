002f1827bfccb86ac96fe1db0a12013d065db7b7
domHenin <domhenin.517@gmail.com>
2019-02-07 11:23:33 -0500

    watched constructor tutorial

--- constructorPractice/MainConstructor.java
+++ constructorPractice/MainConstructor.java
@@ -1,58 +1,25 @@
-class Vehicle {
-    private String carModel;
-    private String carMake;
-    private String carColor;
+class Machine {
+    private String name;
+    private int number;
 
-    public Vehicle() {
-        carModel = "Acura";
-        carMake = "RSX";
-        carColor = "White";
+    public Machine() {
+        name = "Arlo";
+        number = 00;
+        System.out.println("First Constructor Name: "+name+"  and Number: "+number);
     }
 
-    public Vehicle(String carModel, String carMake, String carColor){
-        this.carModel = carModel;
-        this.carMake = carMake;
-        this.carColor = carColor;
-    }
-
-    public String getCarModel(){
-        return carModel;
-    }
-
-    public String getCarMake(){
-        return carMake;
-    }
-
-    public String getCarColor() {
-        return carColor;
-    }
-
-    public void changeCarModel(String newCarModel){
-        carModel = newCarModel;
-    }
-
-    public void changeCarColor(String newCarColor) {
-        carColor = newCarColor;
+    public Machine(String name, int number) {
+        this.name = name;
+        this.number = number;
+        System.out.println("Second Constructor Name: "+name+"  and Number: "+number);
     }
 }
 
 
 public class MainConstructor {
     public static void main(String[] args) {
-//      System.out.println("hello world!");
-
-        Vehicle vehicle = new Vehicle();
-
-      System.out.println("Car Model: "+vehicle.getCarModel());
-      System.out.println("Car Make: "+vehicle.getCarColor());
-      System.out.println("Car Color: "+vehicle.getCarColor());
-
-        System.out.println("I want to change my cars color.....");
-        System.out.println("... Processing ...");
-
-        vehicle.changeCarColor("Black");
-        System.out.println("New Car Color: "+vehicle.getCarColor());
-
+        Machine machine = new Machine();
+        Machine machine1 = new Machine("R2D2", 01);
     }
 }
 
