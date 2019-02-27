dc3c5ea5c095832066518f59015678becc6d11e7
domHenin <domhenin.517@gmail.com>
2019-02-27 09:52:25 -0500

    sorted maps tutorial

---
+++ src/com/caveofprogramming/tutorials/collections/SortedMapsMain.java
@@ -0,0 +1,30 @@
+package com.caveofprogramming.tutorials.collections;
+
+import java.util.*;
+
+
+public class SortedMapsMain {
+    public static void main(String[] args) {
+//      System.out.println("to GOD all glory!");
+        Map<Integer, String> hashMap = new HashMap<>();
+        LinkedHashMap<Integer, String> linkedHashMap = new LinkedHashMap<>();
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+
+
+
+    }
+
+    public static void testMap(Map<Integer, String> map) {
+        map.put(9, "fox");
+        map.put(4, "cat");
+        map.put(8, "dog");
+        map.put(1, "giraffe");
+        map.put(0, "swan");
+        map.put(15, "bear");
+        map.put(6, "snake");
+
+        for(Integer key : map.keySet()) {
+
+        }
+    }
+}
