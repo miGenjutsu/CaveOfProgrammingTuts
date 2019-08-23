ac9bb2b2eb4180e03a3b980cf6951f749e365c19
domHenin <domhenin.517@gmail.com>
2019-08-23 10:57:42 -0400

    completed video

--- src/com/caveofprogramming/tutorials/reading_files_scanner/App.java
+++ src/com/caveofprogramming/tutorials/reading_files_scanner/App.java
@@ -1,4 +1,28 @@
 package src.com.caveofprogramming.tutorials.reading_files_scanner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = "E:\\CloudLogixTech\\Documents\\DesktopFoloder\\Udemy-Courses\\java-cave-of-programming-tutorials\\CaveOfProgrammingTuts\\src\\com\\caveofprogramming\\tutorials\\reading_files_scanner\\example.txt";
+
+        File textFile = new File(fileName);
+
+        Scanner scanner = new Scanner(textFile);
+        int value = scanner.nextInt();
+        System.out.println("Read value: "+value);
+
+        scanner.nextLine();
+
+        int count = 2;
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+
+            System.out.println(count+": "+line);
+            count++;
+        }
+        scanner.close();
+    }
 }
